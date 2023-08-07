20ebed93fab0af0ca462f65221e1521100a59186
ajrhind <alanah.rhind009@gmail.com>
2023-08-07 21:54:50 +1200

    testd

--- draw_letters.js
+++ draw_letters.js
@@ -24,12 +24,15 @@ function drawLetter(letterData) {
   let size2 = letterData["size"];
   let pos2x = 50  + letterData["offsetx"];
   let pos2y = 150 + letterData["offsety"];
+  let testMcTest = letterData["testing?"]
 
   // draw two circles
   fill(darkBlue);
   ellipse(50, 150, 75, 75);
   fill(lightBlue);
   ellipse(pos2x, pos2y, size2, size2);
+  fill(200,100,100);
+  ellipse(testMcTest,100,50);
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
--- editor.js
+++ editor.js
@@ -7,8 +7,13 @@
 const sliderInfo = [
   ["size",       0, 100],
   ["offsetx",  -30,  30],
-  ["offsety", -100, 100]
+  ["offsety", -100, 100],
+  ["testing?", -20,20]
 ];
+
+
+
+
 
 // PROBABLY DON'T NEED TO EDIT ANYTHING ELSE. STOP HERE.
 
